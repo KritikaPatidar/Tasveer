467933599b5a60d1ff56f7b97ebf8856da850506
Kritika Patidar <rupatidarkritika363@gmail.com>
2024-09-19 08:20:21 +0530

    backend configured

--- tasveer_backend/core/views.py
+++ tasveer_backend/core/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Event, Photo, CustomUser, Photographer
+from .serializers import EventSerializer, PhotoSerializer, UserSerializer, PhotographerSerializer
 
 # Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+class PhotographerViewSet(viewsets.ModelViewSet):
+    queryset = Photographer.objects.all()
+    serializer_class = PhotographerSerializer
+
+class PhotoViewSet(viewsets.ModelViewSet):
+    queryset = Photo.objects.all()
+    serializer_class = PhotoSerializer
--- tasveer_backend/tasveer_backend/urls.py
+++ tasveer_backend/tasveer_backend/urls.py
@@ -15,8 +15,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from core import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'events', views.EventViewSet)
+router.register(r'photos', views.PhotoViewSet)
+router.register(r'photographers', views.PhotographerViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),   # Login/Token obtain
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Refresh token
 ]
